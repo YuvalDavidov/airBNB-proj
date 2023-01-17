2bb08d240e2d7f77e23c3201b446c14f7b99ada7
Yaniv Kela <kianiv94@gmail.com>
2023-01-17 21:42:30 +0200

    added demo data, created stay-service-local

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,18 +4,15 @@ import { CarIndex } from './pages/car-index.jsx'
 import { ReviewIndex } from './pages/review-index.jsx'
 import { ChatApp } from './pages/chat-app.jsx'
 import { AdminApp } from './pages/admin-app.jsx'
+import { StayIndex } from './pages/stay-index.jsx'
 
 // Routes accesible from the main navigation (in AppHeader)
 const routes = [
+
     {
         path: '/',
-        component: <HomePage />,
-        label: 'Home 🏠',
-    },
-    {
-        path: 'car',
-        component: <CarIndex />,
-        label: 'Cars'
+        component: <StayIndex />,
+        label: 'Stays'
     },
     {
         path: 'review',
---
+++ frontend/src/services/stay.service.local.js
@@ -0,0 +1,75 @@
+
+import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
+import { userService } from './user.service.js'
+
+const STORAGE_KEY = 'stayDB'
+
+export const stayService = {
+    query,
+    getById,
+    save,
+    remove,
+    getEmptyStay,
+    addStayMsg
+}
+// window.cs = stayService
+
+
+// async function query(filterBy = { txt: '', price: 0 }) {
+async function query() {
+    var stays = await storageService.query(STORAGE_KEY)
+    return stays
+}
+
+function getById(stayId) {
+    return storageService.get(STORAGE_KEY, stayId)
+}
+
+async function remove(stayId) {
+    // throw new Error('Nope')
+    await storageService.remove(STORAGE_KEY, stayId)
+}
+
+async function save(stay) {
+    var savedStay
+    if (stay._id) {
+        savedStay = await storageService.put(STORAGE_KEY, stay)
+    } else {
+        // Later, owner is set by the backend
+        stay.owner = userService.getLoggedinUser()
+        savedStay = await storageService.post(STORAGE_KEY, stay)
+    }
+    return savedStay
+}
+
+async function addStayMsg(stayId, txt) {
+    // Later, this is all done by the backend
+    const stay = await getById(stayId)
+    if (!stay.msgs) stay.msgs = []
+
+    const msg = {
+        id: utilService.makeId(),
+        by: userService.getLoggedinUser(),
+        txt
+    }
+    stay.msgs.push(msg)
+    await storageService.put(STORAGE_KEY, stay)
+
+    return msg
+}
+
+function getEmptyStay() {
+    return {
+        vendor: 'Susita-' + (Date.now() % 1000),
+        price: utilService.getRandomIntInclusive(1000, 9000),
+    }
+}
+
+
+// TEST DATA
+// storageService.post(STORAGE_KEY, {vendor: 'Subali Rahok 2', price: 980}).then(x => console.log(x))
+
+
+
+
