ab4b42f42c50ca7d168b14b50a8355cb953afd57
Shaked Golan <shaked518@gmail.com>
2023-01-19 15:16:23 +0200

    for demo with yaron

---
+++ frontend/src/cmps/date-filter.jsx
@@ -0,0 +1,21 @@
+import { StayMap } from "../cmps/stay-map";
+import { StayDatePicker } from "../cmps/stay-date-picker";
+
+export function DateFilter() {
+
+    return (
+        <section className="place-modal">
+            <ul>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+            </ul>
+        </section>
+    )
+}
---
+++ frontend/src/cmps/guest-filter.jsx
@@ -0,0 +1,18 @@
+export function GuestFilter() {
+
+    return (
+        <section className="place-modal">
+            <ul>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+            </ul>
+        </section>
+    )
+}
